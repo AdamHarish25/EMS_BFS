c914e2ed28d31db7e572817c534e1bc98289be2c
AdamHarish25 <adamharits25@gmail.com>
2026-05-15 10:33:49 +0700

    chore: add 10.165.40.127 to allowed development origins in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: ['192.168.88.252', '192.168.8.156', '127.0.0.1', 'localhost'],
+    allowedDevOrigins: ['192.168.88.252', '192.168.8.156', '127.0.0.1', 'localhost', '10.165.40.127'],
 };
 
 export default nextConfig;
